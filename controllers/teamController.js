2112545b07b2f8fc109d31a1d606dfeeb6730d57
Nilou Mostofi <nilou@km-direct.com>
2020-11-17 13:02:50 -0800

    team-controller

---
+++ controllers/teamController.js
@@ -0,0 +1,26 @@
+const db = require('../models');
+
+module.exports = {
+  findAll: function (req, res) {
+    db.Post.find(req.query)
+      .sort({date: -1})
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.Team.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Team.findOneAndUpdate({_id: req.params.id}, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    db.Team.findById({_id: req.params.id})
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
