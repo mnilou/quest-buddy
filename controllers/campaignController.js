617d5abea9d3c5cc8d399b8f542ad9ea545b14da
Nilou Mostofi <nilou@km-direct.com>
2020-11-17 09:59:46 -0800

    campagin-controller

---
+++ controllers/campaignController.js
@@ -0,0 +1,23 @@
+const db = require("../models");
+
+module.exports = { 
+    create: function(req, res) {
+        db.Campaign
+          .create(req.body)
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      },
+      update: function(req, res) {
+        db.Campaign
+          .findOneAndUpdate({ _id: req.params.id }, req.body)
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      },
+      remove: function(req, res) {
+        db.Campaign
+          .findById({ _id: req.params.id })
+          .then(dbModel => dbModel.remove())
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      }
+};
