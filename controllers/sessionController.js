e216789a3e8039231bafec165dc47eb0c3b7069f
Nilou Mostofi <nilou@km-direct.com>
2020-11-17 12:58:48 -0800

    session-controller

---
+++ controllers/sessionController.js
@@ -0,0 +1,20 @@
+const db = require('../models');
+
+module.exports = {
+  create: function (req, res) {
+    db.Session.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Session.findOneAndUpdate({_id: req.params.id}, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    db.Session.findById({_id: req.params.id})
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+};
