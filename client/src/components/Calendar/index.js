30c652b2da56bc0296c6dab97afa8b57d54459e6
Nilou Mostofi <nilou@km-direct.com>
2020-11-18 13:00:33 -0800

    calendar component

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -32,13 +32,14 @@ export default class Calendar extends React.Component {
             selectMirror={true}
             dayMaxEvents={true}
             weekends={this.state.weekendsVisible}
+            events={this.state.events}
             initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
             select={this.handleDateSelect}
             eventContent={renderEventContent} // custom render function
             eventClick={this.handleEventClick}
             eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+            eventAdd={function(){}}
             /* you can update a remote database when these fire:
-                eventAdd={function(){}}
                 eventChange={function(){}}
                 eventRemove={function(){}}
                 */
