3501dc1274f93ca861a7ddaa11d3c6120b323767
Chris C <christopher.cruzcosa@gmail.com>
2020-11-19 10:55:51 -0800

    added params func

--- client/src/pages/UserPage/index.js
+++ client/src/pages/UserPage/index.js
@@ -1,7 +1,8 @@
-import { React, useHistory } from "react-router-dom";
+import { React, useHistory, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useAuth } from "../../util/authContext";
 function UserPage() {
+    const {id} = useParams();
     const history = useHistory();
     const { user } = useAuth();
     const characterCreatorClick = (event) => {
