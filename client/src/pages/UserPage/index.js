b3e94f8b330c0bfcbb18a0c9574a5ae400101bc3
Chris C <christopher.cruzcosa@gmail.com>
2020-11-22 12:14:43 -0800

    minor changes to button and logo showup

--- client/src/pages/UserPage/index.js
+++ client/src/pages/UserPage/index.js
@@ -45,7 +45,7 @@ function UserPage() {
 
     return (
         <main className="container">
-            <h3 className="mt-3 mb-4 text-center">Welcome home, <span style={{ color: "red" }}>{user.username}</span></h3>
+            <h3 className="mt-5 mb-4 text-center">Welcome home, <span style={{ color: "red" }}>{user.username}</span></h3>
             <div className="row">
                 <div className="col-md-5 mt-2 componentLeft border">
                     <div className="row justify-content-center">
@@ -85,7 +85,7 @@ function UserPage() {
                         <h4 className="my-3 ml-3 text-center">My Teams</h4>
                     </div>
                     <div className="row">
-                        <button type="button" className="btn btn-outline-purple btn-block mx-3 mt-3 mb-1"><Link target="_blank" to="/team_search">Join an Existing Team</Link></button>
+                        <button type="button" className="btn btn-outline-primary btn-block mx-3 mt-3 mb-1"><Link target="_blank" to="/team_search">Join an Existing Team</Link></button>
                     </div>
                     <div className="row">
                         <button type="button" className="btn btn-outline-success btn-block mx-3 mb-2" onClick={teamCreatorClick}>Make a New Team</button>
